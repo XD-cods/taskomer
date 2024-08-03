3e88f440a695a1f0eab3545a3c1e5d68b9b9060c
vladkuzhyr <vkuzir7@gmail.com>
2024-08-03 10:35:03 +0300

    init project

---
+++ src/main/java/com/example/taskomer/TaskomerApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskomerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaskomerApplication.class, args);
+  }
+
+}
