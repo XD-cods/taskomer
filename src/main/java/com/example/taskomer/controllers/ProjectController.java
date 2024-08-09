e08d0a40deba4741fdc2d12b7452d366d2d5ff35
vladkuzhyr <vkuzir7@gmail.com>
2024-08-09 17:13:03 +0300

    merge endpoints for update and create in ProjectController

--- src/main/java/com/example/taskomer/controllers/ProjectController.java
+++ src/main/java/com/example/taskomer/controllers/ProjectController.java
@@ -1,7 +1,6 @@
 package com.example.taskomer.controllers;
 
 import com.example.taskomer.DTO.ProjectDTO;
-import com.example.taskomer.exceptions.BadRequestException;
 import com.example.taskomer.exceptions.NotFoundException;
 import com.example.taskomer.model.Project;
 import com.example.taskomer.repositories.ProjectRepo;
@@ -10,12 +9,12 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -28,39 +27,25 @@ public class ProjectController {
   private final ProjectRepo projectRepo;
   private final ProjectMapper projectMapper;
 
-  public static final String CREATE_PROJECT = "/api/projects";
   public static final String SHOW_PROJECTS = "/api/projects";
   public static final String DELETE_PROJECT = "/api/projects/{id}";
-  public static final String CREATE_OR_UPDATE_PROJECT = "/api/projects/{id}";
+  public static final String CREATE_OR_UPDATE_PROJECT = "/api/projects";
 
 
-  @PostMapping(CREATE_PROJECT)
-  public ProjectDTO createProject(@RequestParam("project_name") String projectName) {
-    projectRepo.findByProjectName(projectName)
-            .ifPresent(project -> {
-              throw new BadRequestException("Project already exists");
-            });
+  @PostMapping(CREATE_OR_UPDATE_PROJECT)
+  public ProjectDTO updateProject(@RequestParam("project_name") String projectName,
+                                  @RequestParam(value = "project_id", required = false) Optional<Long> projectId) {
 
-    Project project = new Project();
+    Project project = projectId
+            .map(id -> projectRepo.findById(id).get())
+            .orElseGet(() -> Project.builder()
+                    .createdAt(Instant.now())
+                    .build());
+
     project.setProjectName(projectName);
     return projectMapper.toDto(projectRepo.save(project));
   }
 
-//  @PatchMapping(CREATE_OR_UPDATE_PROJECT)
-//  public ProjectDTO updateProject(@RequestParam(value = "optionalProjectId",required = false) Optional<Long> optionalProjectId,
-//                                  @RequestParam(value = "optionalProjectName",required = false) Optional<String> optionalProjectName) {
-//    optionalProjectName = optionalProjectName.filter(projectName -> !projectName.trim().isEmpty());
-//
-//    boolean isCreated = optionalProjectId.isPresent();
-//    if (!isCreated && !optionalProjectName.isPresent()) {
-//      throw new BadRequestException("Project name is mandatory");
-//    }
-//
-//    Project project = optionalProjectId
-//            .map(projectRepo::findById)
-//            .get()
-//            .orElseGet(() -> Project.builder().build());
-//  }
 
   @GetMapping(SHOW_PROJECTS)
   public List<ProjectDTO> getAllProjects(
@@ -76,6 +61,7 @@ public class ProjectController {
             .map(projectMapper::toDto)
             .collect(Collectors.toList());
   }
+
 
   @DeleteMapping(DELETE_PROJECT)
   public Boolean deleteProject(@PathVariable("id") Long id) {
